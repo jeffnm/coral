cd8615923b8671e9732f0c44f1902c8078fb6502
jeffnm <jeffmudge@gmail.com>
2019-02-14 17:00:00 -0600

    clean up a notice, reduce versions to test while developing, copy config files before testing

--- common/common_directory.php
+++ common/common_directory.php
@@ -100,11 +100,10 @@ $GLOBALS['http_lang'] = NULL;
 
 if(isset($_COOKIE["lang"])) {
 	$GLOBALS['http_lang'] = $_COOKIE["lang"];
-} else {
+} elseif(isset($_SERVER["HTTP_ACCEPT_LANGUAGE"])){
 	$codeL = str_replace("-","_",substr($_SERVER["HTTP_ACCEPT_LANGUAGE"],0,5));
 	$GLOBALS['http_lang'] = $lang_name->getLanguage($codeL);
-
-	if($GLOBALS['http_lang'] == "") {
+} else {($GLOBALS['http_lang'] == "") {
 		$GLOBALS['http_lang'] = "en_US";
 	}
 }
