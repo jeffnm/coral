c60c5cbffe9d9aca4ef29f5996fa3b33be4a5cd5
jeffnm <jeffmudge@gmail.com>
2019-02-15 14:45:35 -0600

    change db setup back to test and implement environment switcher

--- phpunittests/Test.php
+++ phpunittests/Test.php
@@ -3,10 +3,33 @@ use PHPUnit\Framework\TestCase;
 
 class UserTest extends TestCase
 {
-    public function testThatWeCanGetUserArray()
-    {
-        $user = new User;
+// functions used in setup and tear down
+  public function switchToTestEnvironment() {
+    $config = new \Config_Lite("admin/configuration.ini");
+    $config->set("settings", "environment", "test");
+    $config->save();
+  }
 
-        $this->assertInternalType('array', $user->allAsArray);
-    }
+  public function switchBackToProdEnvironment() {
+    $config = new \Config_Lite("admin/configuration.ini");
+    $config->set("settings", "environment", "prod");
+    $config->save();
+  }
+
+// once at the beginning before any tests
+  protected function setUpBeforeClass(){
+    $this->switchToTestEnvironment();
+  }
+
+// once at the end after tests (why? not sure it's needed since build is destroyed after)
+  protected function tearDownAfterClass(){
+    $this->switchToProdEnvironment
+  }
+
+// begin actual tests
+  public function testThatWeCanGetUserArray(){
+    $user = new User;
+
+    $this->assertInternalType('array', $user->allAsArray);
+  }
 }
