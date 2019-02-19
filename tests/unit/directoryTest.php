c6b4f6a55b22bed2f0be813aaeaf59c9398cc2ad
Matthias Meusburger <matthias.meusburger@biblibre.com>
2019-02-19 10:43:03 +0100

    POC: Unit tests

---
+++ tests/unit/directoryTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require "../../resources/directory.php";
+
+use PHPUnit\Framework\TestCase;
+
+class DirectoryTest extends TestCase
+{
+    public function testCostToInteger() {
+        $this->assertSame(1052.0, cost_to_integer(10.52));
+    }
+
+    public function testIntegerToCost() {
+        // Integer to cost actually returns a string
+        $this->assertSame('10.52', integer_to_cost(1052));
+    }
+
+    public function testPreviousYear() {
+        $this->assertSame('1998', previous_year(1999));
+    }
+
+    public function testPreviousYear18xx() {
+        // Doesn't work with years < 1900
+        //$this->assertSame('1819', previous_year(1820));
+    }
+}
